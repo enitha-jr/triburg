3bc17a7f393006aa445a80d6090f8f9cbc146672
enitha-jr <enitharajeshkumar@gmail.com>
2025-06-24 06:47:36 +0530

    landing

---
+++ triburg/src/components/Hero.js
@@ -0,0 +1,122 @@
+"use client";
+
+import { useEffect } from 'react';
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+import '../styles/hero.css';
+
+// import redCloth from "../assets/images/redCloth.jpg";
+// import yarns from "../assets/images/yarns.jpg";
+// import hanger from "../assets/images/hanger.jpg";
+// import denim from "../assets/images/denim.jpg";
+// import scrubs from "../assets/images/scrubs.jpg";
+
+
+export default function Hero() {
+  useEffect(() => {
+    new Swiper('.swiper', {
+      effect: 'coverflow',
+      grabCursor: true,
+      centeredSlides: true,
+      autoplay: {
+        delay: 1200, 
+        disableOnInteraction: false, 
+      },
+      coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 100,
+        modifier: 3,
+        slideShadows: true,
+      },
+      keyboard: {
+        enabled: true,
+      },
+      mousewheel: {
+        thresholdDelta: 70,
+      },
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      breakpoints: {
+        640: {
+          slidesPerView: 2,
+        },
+        768: {
+          slidesPerView: 1,
+        },
+        1024: {
+          slidesPerView: 2,
+        },
+        1560: {
+          slidesPerView: 3,
+        },
+      },
+    });
+  }, []);
+
+  return (
+    <main>
+      <div className ="w-4xl"> 
+        <h1>RSK WEAVERS</h1>
+        <div className="hr-span">
+          <hr />
+          <span>TRIBURG</span>
+        </div>
+        <p>Discover a world of quality yarns, fabrics, and innovation. 
+          From couture to bulk manufacturing — Your trusted gateway to premium fabrics.</p>
+      </div>
+
+      <div className="swiper">
+        <div className="swiper-wrapper">
+          <div className="swiper-slide swiper-slide--one">
+            <div>
+              <h2>Jellyfish</h2>
+              <p>Jellyfish and sea jellies are gelatinous medusa-phase members of the phylum Cnidaria.</p>
+              <a href="https://en.wikipedia.org/wiki/Jellyfish" target="_blank" rel="noopener noreferrer">explore</a>
+            </div>
+          </div>
+
+          <div className="swiper-slide swiper-slide--two">
+            <div>
+              <h2>Seahorse</h2>
+              <p>Found in shallow tropical and temperate waters. Four species are found in Pacific waters.</p>
+              <a href="https://en.wikipedia.org/wiki/Seahorse" target="_blank" rel="noopener noreferrer">explore</a>
+            </div>
+          </div>
+
+          <div className="swiper-slide swiper-slide--three">
+            <div>
+              <h2>Octopus</h2>
+              <p>Octopuses inhabit coral reefs, deep oceans, and intertidal zones. Most grow fast and live short.</p>
+              <a href="https://en.wikipedia.org/wiki/Octopus" target="_blank" rel="noopener noreferrer">explore</a>
+            </div>
+          </div>
+
+          <div className="swiper-slide swiper-slide--four">
+
+            <div>
+              <h2>Shark</h2>
+              <p>Sharks are cartilaginous fish with gill slits, pectoral fins, and are powerful marine predators.</p>
+              <a href="https://en.wikipedia.org/wiki/Shark" target="_blank" rel="noopener noreferrer">explore</a>
+            </div>
+          </div>
+
+          <div className="swiper-slide swiper-slide--five">
+              <div>
+              <h2>Dolphin</h2>
+              <p>Dolphins live in warm or cold waters and feed on fish, squid, and occasionally marine mammals.</p>
+              <a href="https://en.wikipedia.org/wiki/Dolphin" target="_blank" rel="noopener noreferrer">explore</a>
+            </div>
+          </div>
+        </div>
+        <div className="swiper-pagination"></div>
+      </div>
+
+      <img src="https://cdn.pixabay.com/photo/2021/11/04/19/39/jellyfish-6769173_960_720.png" alt="" className="bg" />
+      <img src="https://cdn.pixabay.com/photo/2012/04/13/13/57/scallop-32506_960_720.png" alt="" className="bg2" />
+    </main>
+  );
+}  
---
+++ triburg/src/components/Youcanbuy.js
@@ -0,0 +1,87 @@
+"use client";
+
+import React, { useEffect } from "react";
+import "../styles/Youcanbuy.css";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const Youcanbuy = () => {
+  useEffect(() => {
+    const items = gsap.utils.toArray("ul li");
+
+    gsap.set(items, { opacity: (i) => (i !== 0 ? 0.2 : 1) });
+
+    const dimmer = gsap
+      .timeline()
+      .to(items.slice(1), {
+        opacity: 1,
+        stagger: 0.5,
+      })
+      .to(
+        items.slice(0, items.length - 1),
+        {
+          opacity: 0.2,
+          stagger: 0.5,
+        },
+        0
+      );
+
+    ScrollTrigger.create({
+      trigger: items[0],
+      endTrigger: items[items.length - 1],
+      start: "center center",
+      end: "center center",
+      animation: dimmer,
+      scrub: 0.2,
+    });
+
+    gsap.fromTo(
+      document.documentElement,
+      {
+        "--hue": 0,
+      },
+      {
+        "--hue": 360,
+        ease: "none",
+        scrollTrigger: {
+          trigger: items[0],
+          endTrigger: items[items.length - 1],
+          start: "center center",
+          end: "center center",
+          scrub: 0.2,
+        },
+      }
+    );
+  }, []);
+
+  return (
+    <main>
+      <section className="content fluid w-1/2">
+        <h2>
+          <span aria-hidden="true">you can buy&nbsp;</span>
+          {/* <span className="sr-only">you can ship things.</span> */}
+        </h2>
+        <ul aria-hidden="true" style={{ "--count": 10 }}>
+          <li style={{ "--i": 0 }}>design.</li>
+          <li style={{ "--i": 1 }}>prototype.</li>
+          <li style={{ "--i": 2 }}>solve.</li>
+          <li style={{ "--i": 3 }}>build.</li>
+          <li style={{ "--i": 4 }}>develop.</li>
+          <li style={{ "--i": 5 }}>debug.</li>
+          <li style={{ "--i": 6 }}>learn.</li>
+          <li style={{ "--i": 7 }}>cook.</li>
+          <li style={{ "--i": 8 }}>ship.</li>
+          <li style={{ "--i": 9 }}>prompt.</li>
+          <li style={{ "--i": 10 }}>collaborate.</li>
+        </ul>
+      </section>
+      <section>
+        <h2 className="fluid">second section - images</h2>
+      </section>
+    </main>
+  );
+};
+
+export default Youcanbuy;
